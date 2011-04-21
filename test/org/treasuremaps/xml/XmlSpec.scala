96da26d846d3497f527de7a31228d63915aa44ba
adriancowham <adrian@zoodles.com>
2011-04-20 22:37:01 -0700

    added the scalatest lib

---
+++ test/org/treasuremaps/xml/XmlSpec.scala
@@ -0,0 +1,9 @@
+package org.treasuremaps.xml
+
+import org.scalatest.Spec
+import org.scalatest.matchers.ShouldMatchers
+import scala.collection.mutable.Stack
+
+class XmlSpec extends Spec with ShouldMatchers {
+
+}
