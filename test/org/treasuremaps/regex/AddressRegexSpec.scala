b8238805d5068717e72fada5c4e82930e3a4aaeb
adriancowham <adrian@zoodles.com>
2011-04-24 13:16:18 -0700

    working on an Address regex, created a data file for expected values only for posts that have street addresses ending in Way

--- test/org/treasuremaps/regex/AddressRegexSpec.scala
+++ test/org/treasuremaps/regex/AddressRegexSpec.scala
@@ -1,12 +1,11 @@
 package org.treasuremaps.regex
 
-import org.scalatest.junit.JUnitRunner
-
 import org.junit.runner.RunWith
-
-import scala.util.matching.Regex
-import org.scalatest.matchers.ShouldMatchers
 import org.scalatest.Spec
+import org.scalatest.junit.JUnitRunner
+import org.scalatest.matchers.ShouldMatchers
+import scala.io.Source
+import scala.util.matching.Regex
 import scala.xml.XML
 
 @RunWith( classOf[ JUnitRunner ] )
@@ -14,16 +13,25 @@ class AddressRegexSpec extends Spec with ShouldMatchers {
 
 	describe( "Address Regular Expressions" ) {
 		
+		val expected = Source.fromFile( "data/addresses/ends_with_way_expected.txt" ).getLines.toArray
+				
+		val addyRegex = new Regex( """(?s).*(\d{4} .* [Ww]ay).*""" );
+		val feed = XML loadFile( "data/addresses/ends_with_way.xml" );
+		val posts = feed \ "item"
+		
 		it( "should parse for addresses ending in way" ) {
-			val feed = XML loadFile( "data/addresses/ends_with_way.xml" )
 			
-			val desc = ( feed \ "description" ) text
-			val AddyRegex = new Regex( """(?s).*(\d{4} .* [Ww]ay).*""" );
-			desc match {
-				case AddyRegex( addy ) => addy should equal( "1818 Irongate Way" ) 
-				case _ => fail
-			}			
+			// make sure the data sets have the same length
+			expected.length should equal( posts.length )
+			var i = 0
+			for( item <- posts \ "description" ) {
+				
+				item text match {
+					case addyRegex( addy ) => addy should equal( expected( i ) ) 
+					case _ => fail
+				}
+				i += 1
+			}
 		}
 	}
-			
 }
