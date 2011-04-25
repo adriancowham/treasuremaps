b961b9b7d7c401eedface1a9c339de3462f7dbde
adriancowham <adrian@zoodles.com>
2011-04-24 22:49:34 -0700

    modified one of the regexs to handle a variable length street number

--- test/org/treasuremaps/regex/AddressRegexSpec.scala
+++ test/org/treasuremaps/regex/AddressRegexSpec.scala
@@ -11,17 +11,43 @@ import scala.xml.XML
 @RunWith( classOf[ JUnitRunner ] )
 class AddressRegexSpec extends Spec with ShouldMatchers {
 
-	describe( "Address Regular Expressions" ) {
+	describe( "Addresses with Way regular expressions" ) {
 		
-		val expected = Source.fromFile( "data/addresses/ends_with_way_expected.txt" ).getLines.toArray
+		val expected = Source.fromFile( "data/addresses/addresses_with_way_expected.txt" ).getLines.toArray
 				
-		val addyRegex = new Regex( """(?s).*(\d{4} .* [Ww]ay).*""" );
-		val feed = XML loadFile( "data/addresses/ends_with_way.xml" );
+		val addyRegex = new Regex( """(?s).*(\b\d.*[Ww][Aa][Yy]).*""" );
+		val feed = XML loadFile( "data/addresses/addresses_with_way.xml" );
 		val posts = feed \ "item"
 		
-		it( "should parse for addresses ending in way" ) {
+		it( "should parse for addresses on a Way" ) {
 			
 			// make sure the data sets have the same length
+			expected.length should equal( posts.length )
+			var i = 0
+			for( item <- posts \ "description" ) {
+				
+				item text match {
+					case addyRegex( addy ) => {
+						println( addy )
+						addy should equal( expected( i ) ) 
+					}
+					case _ => fail
+				}
+				i += 1
+			}
+		}
+	}
+	
+	describe( "Addresses with Drive regular expressions" ) {
+				
+		val expected = Source.fromFile( "data/addresses/addresses_with_drive_expected.txt" ).getLines.toArray
+
+		val addyRegex = new Regex( """(?s).*(\d{3} .* [Dd][Rr][Ii][Vv][Ee]).*""" );
+		val feed = XML loadFile( "data/addresses/addresses_with_drive.xml" );
+		val posts = feed \ "item"
+		
+		it( "should parse for addresses on a Drive" ) {
+						// make sure the data sets have the same length
 			expected.length should equal( posts.length )
 			var i = 0
 			for( item <- posts \ "description" ) {
@@ -34,4 +60,27 @@ class AddressRegexSpec extends Spec with ShouldMatchers {
 			}
 		}
 	}
+	
+	describe( "Addresses with Lane regular expressions" ) {
+				
+		val expected = Source.fromFile( "data/addresses/addresses_with_lane_expected.txt" ).getLines.toArray
+
+		val addyRegex = new Regex( """(?s).*(\d{3} .* [Ll][Aa][Nn][Ee]).*""" );
+		val feed = XML loadFile( "data/addresses/addresses_with_lane.xml" );
+		val posts = feed \ "item"
+		
+		it( "should parse for addresses on a Lane" ) {
+						// make sure the data sets have the same length
+			expected.length should equal( posts.length )
+			var i = 0
+			for( item <- posts \ "description" ) {
+				
+				item text match {
+					case addyRegex( addy ) => addy should equal( expected( i ) ) 
+					case _ => fail
+				}
+				i += 1
+			}
+		}
+	}	
 }
