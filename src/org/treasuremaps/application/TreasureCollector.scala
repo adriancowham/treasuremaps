e53d5fd4ae4bad9a3ec5709e4cb9665295444ff1
brendanobra <brendan@obrafamily.org>
2011-05-31 20:30:31 -0700

    checkpoint

--- src/org/treasuremaps/application/TreasureCollector.scala
+++ src/org/treasuremaps/application/TreasureCollector.scala
@@ -28,9 +28,12 @@ class TreasureCollector {
 		val visited = new HashSet[Node]()
 		// for every "post" in the feed, try to match its description against a
 		// regular expression
+		var total = new java.lang.Integer( 0 )
+		var misses = new java.lang.Integer( 0 )
 		for( post <- posts \ "description" ) {
 			// TODO: create filenames based on date/time for the current day and the type of 
 			// street qualifier
+		
 			post text match {
 				case AddressRegex.FullyQualifiedWay( addy ) => { 
 						appendToFile( post, generateFilename( "ways" ) )
