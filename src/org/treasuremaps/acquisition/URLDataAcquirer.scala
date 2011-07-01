d7e84a8637c9c4f686fb3cdfd29485f70694f699
adriancowham <adrian@zoodles.com>
2011-06-30 22:08:10 -0700

    formatting

--- src/org/treasuremaps/acquisition/URLDataAcquirer.scala
+++ src/org/treasuremaps/acquisition/URLDataAcquirer.scala
@@ -7,8 +7,8 @@ import java.net.URL
  * "Production" data acquirer... given the injected url, get it and return as @Elem
  * 
  */
-class URLDataAcquirer(url: String) extends FeedAcquirer {
+class URLDataAcquirer( url: String ) extends FeedAcquirer {
 
-  def acquire(): Elem = { XML load (new URL( url ).openConnection.getInputStream)}
+  def acquire() : Elem = XML.load ( new URL( url ).openConnection.getInputStream )
 
 }
